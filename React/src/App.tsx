a6206482e400a02825b3cb7e4e90da7c8904592d
AhmedBassoul1 <ahmed@cylindrique.com>
2026-09-13 14:11:04 +0100

    Implement todo list display component

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,10 +1,19 @@
+import { dummydata } from "./assets/data/todos";
+
 function App() {
 
   return (
     <main className="py-10 px-2">
     <h1 className="text-center text-3xl font-bold underline">
-      Hello world!
+      My Todo List
     </h1>  
+    <div className="max-w-md mx-auto">
+      { dummydata.map(todo =>(
+        <p key={todo.id} className="bg-gray-200 p-4 rounded mb-2 text-lg">
+          {todo.title}
+        </p>
+      ))}
+    </div>
     </main>
   )
 }
